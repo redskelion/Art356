c2c7b4632733728bd49b7faebc918c1e2d6d8b6f
Diego <lazer.trainer@gmail.com>
2026-09-15 17:34:10 -0700

    Add files via upload

--- demo/sketch.js
+++ demo/sketch.js
@@ -1,11 +1,10 @@
 function setup() {
-createCanvas(600, 600);
+let canvas = createCanvas(600, 600);
+  canvas.parent('id');
+  
     background(220);
 }
 
 function draw() {
-
-
-
   ellipse(mouseX, mouseY, 20);
 }
